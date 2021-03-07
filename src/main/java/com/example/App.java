bd05afecc9584b52e9a9e7e4a3f081c3b29eccd1
30047778 <79073983+30047778@users.noreply.github.com>
2021-03-07 10:24:30 +0900

    Update App.java
    ori

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,13 +10,3 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
-
-}		
-	public static void main(String[] args) {	
-		setArray();
-	}	
-	public static void setArray(){	
-		int list[] = new int[10]; 
-		list[20] = 1;
-	}	
-}		
