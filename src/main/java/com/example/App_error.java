1748da4dbf2fa0c4e5412e1b28bad33520cdbe5b
30047778 <79073983+30047778@users.noreply.github.com>
2021-03-07 10:49:50 +0900

    Delete App_error.java
    del

--- src/main/java/com/example/App_error.java
+++
@@ -1,7 +0,0 @@
-package com.example;		
-public class SampleClass {		
-	public static void main(String[] args) {	
-		String buf = null;
-		System.out.println("buf length is " + buf.length());
-	}	
-}		
